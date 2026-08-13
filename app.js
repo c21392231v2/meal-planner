fc2b4a7faae9bca2a0743f98a3473cfcd01d5802
c21392231v2 <c21392231@mytudublin.ie>
2026-08-13 21:52:25 +0100

    Setup server for meal planner

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Meal Planner is running');
+});
+app.listen(3000, () => {
+    console.log('Express server running at http://localhost:3000/');
+});
