5cf3585abec776be8d04aca2d2405457c425c361
c21392231v2 <c21392231@mytudublin.ie>
2026-08-15 15:41:35 +0100

    Add database and config

---
+++ database/schema.sql
@@ -0,0 +1,22 @@
+create database mealie
+character set utf8mb4
+collate utf8mb4_unicode_ci;
+
+use mealie;
+
+create table users (
+	user_id int auto_increment primary key,
+    username varchar(100) not null,
+    email varchar(150) not null unique,
+    password_hash varchar(255) not null
+);
+
+create table meals (
+	meal_id int auto_increment primary key,
+	user_id int not null,
+    title varchar(150) not null,
+    meal_day varchar(50),
+    time_of_day varchar(50),
+    foreign key (user_id) references users(user_id)
+);    
+
