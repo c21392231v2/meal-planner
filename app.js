fcf85181bfd17a7ca5fbb45787a82377abae0e91
c21392231v2 <c21392231@mytudublin.ie>
2026-08-15 01:46:35 +0100

    Add register, login and dashboard

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('home', { title: 'Mealie - Your Meal Planner' });
 });
-app.listen(3000, () => {
-    console.log('Express server running at http://localhost:3000/');
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', { title: 'Dashboard' });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', { title: 'Register' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server running at http://localhost:${PORT}/`);
 });
